d31b754856046414426b6e9e40ab230b82af5984
slardeux <slardeux@yahoo.fr>
2015-03-11 14:45:32 -0400

    initial commit code to get the data from nycdsa webiste and probably others if txt and csv

---
+++ get_data.R
@@ -0,0 +1,64 @@
+library(dplyr)
+library(RCurl)
+library(XML)
+
+get_url.f <- function(baseurl,rmv = 0,...){
+  urls <- paste(baseurl,..., sep = '/')
+  script <- getURL(urls)
+  doc <- htmlParse(script)
+  links <- getHTMLLinks(doc)
+  if(rmv != 0){links <- links[-rmv]}
+  links <- paste(urls, links, sep = '')
+  return(links)
+}
+get_csv.f <- function(links, folder){
+  csvurl <- links[grep('csv', links)]
+  for(i in 1:length(csvurl)){
+    csvname <- paste(folder,strsplit(csvurl[i], '/')[[1]][length(strsplit(csvurl[i], '/')[[1]])], sep = '/')
+    download.file(url=csvurl[i],destfile=csvname)
+  }
+}
+get_txt.f <- function(links, folder){
+  txturl <- links[grep('txt', links)]
+  for(i in 1:length(txturl)){
+    txtfile <- try(read.table(txturl[i]), silent = TRUE)
+    txtname <- paste(folder,strsplit(txturl[i], '/')[[1]][length(strsplit(txturl[i], '/')[[1]])], sep = '/')
+    if(class(txtfile) == 'try-error'){
+      txtfile <- readLines(txturl[i])
+      writeLines(txtfile, txtname)
+    }else{
+      write.table(txtfile, file = txtname)
+    }
+  }
+}
+
+## main
+baseurl <- 'http://www.nycdatascience.com/slides/BOOTCAMP'
+week <- 'week6'
+lec <- 'lec1'
+folder <- 'classdata'
+dir.create(file.path(folder), showWarnings = FALSE)
+
+links <- get_url.f(baseurl, rmv = 1, week, lec, 'data/' )
+if(any(grepl('csv', links))) get_csv.f(links, folder)
+if(any(grepl('txt', links))) get_txt.f(links, folder)
+
+if(any(substring(links, nchar(links)) == '/')){
+  lk <- links[which(substring(links, nchar(links)) == '/')]
+  while(any(substring(lk, nchar(lk)) == '/')){
+      lk <- lk[which(substring(lk, nchar(lk)) == '/')]
+    for(i in 1:length(lk)){
+      newlk <- get_url.f(lk[i], rmv = 1)
+      subfolder <- paste(folder, strsplit(lk, '/')[[i]][length(strsplit(lk, '/')[[i]])], sep = '/')
+      dir.create(file.path(subfolder), showWarnings = FALSE)
+      if(any(grepl('csv', newlk))) get_csv.f(newlk, subfolder)
+      if(any(grepl('txt', newlk))) get_txt.f(newlk, subfolder)
+    }
+    lk <- newlk
+    folder <- subfolder
+  }
+}
+
+
+
+
